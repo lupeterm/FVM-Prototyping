77daaa888b6d5cacb86b16c4f067a12e6a9cc522
Lukas Petermann <lukas.petermann13@gmail.com>
2026-05-10 19:45:44 +0200

    better warmup

--- MinimalFVM/src/MinimalFVM.jl
+++ MinimalFVM/src/MinimalFVM.jl
@@ -145,9 +145,6 @@ function faceBasedAll(
         vals[rowOwnStart+diagOffs[iOwner]] -= valueUpper
         vals[rowOwnStart+ownOffs[iFace]] += valueLower
         vals[rowNeiStart+diagOffs[iNeighbor]] -= valueLower
-        if rowOwnStart + diagOffs[iOwner] == 1 || rowNeiStart + diagOffs[iNeighbor] == 1
-            println("JULIA (valuemat): $valueUpper, $valueLower")
-        end
     end
     for facei in numInteriorFaces+1:length(faceFlux)
         bcfacei = facei - numInteriorFaces
@@ -168,8 +165,6 @@ function faceBasedAll(
         rowOwnStart = rowOffs[own] + 1
         # operatorScalingOwn = operatorScaling[own]
 
-        # valueMat = flux * operatorScalingOwn * valFrac2
-
         vals[rowOwnStart+diagOffs[own]] += valueDiag
         # bValues[bcfacei] = valueDiag
         # rhs[own] -= valueRhs
@@ -177,21 +172,9 @@ function faceBasedAll(
         RHS[own+numCells] += valueRHSy
         RHS[own+numCells+numCells] += valueRHSz
 
-        
-        if rowOwnStart + diagOffs[own] == 1
-            println("bcface $bcfacei (refValue): $(refValue[start:end_])")
-            println("bcface $bcfacei (refGradient): $(refGradient[start:end_])")
-            println("bcface $bcfacei (valueDiag): $valueDiag")
-        end
         # bRhs[own] += valueRHSx
         # bRhs[own+numCells] += valueRHSy
         # bRhs[own+numCells+numCells] += valueRHSz
-    end
-    for i in 1:4
-        if i > length(vals)
-            break
-        end
-        println("value $i: $(vals[i])")
     end
 end
 
@@ -277,311 +260,35 @@ function faceBasedBoundary(
     end
 end
 
+function warmup(op::String)
+    faceBasedAll(
+        Int32(1),
+        ones(Int32, 2),
+        ones(Int32, 2),
+        ones(UInt8, 2),
+        ones(UInt8, 2),
+        ones(UInt8, 2),
+        ones(Int32, 2),
+        zeros(Float64, 4),
+        op,
+        ones(Float64, 2),
+        ones(Float64, 2),
+        ones(Float64, 2),
+        ones(Float64, 2),
+        ones(Float64, 2),
+        ones(Float64, 3),
+        ones(Float64, 3),
+        zeros(Float64, 9),
+        zeros(Int32, 2),
+    )
+end
+
 
 @compile_workload begin
-    d = eval(Meta.parse("Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0)"))
-    d = eval(Meta.parse("Laplace{Float64}(1.0)"))
-    d = eval(Meta.parse("Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0) + Laplace{Float64}(1.0)"))
-    laplace = Laplace{Float64}(1.0)
-    laplace(1.0, 0.0, 1.0, 0.0, 0.0, 0.0)
-    divU = Div{Float64,upwind{Float64}}(upwind{Float64}(), 1.0)
-    divU(1.0, 0.0, 1.0, 0.0, 0.0, 0.0)
-    divULap = divU + laplace
-    divL = Div{Float64,linear{Float64}}(linear{Float64}(), 1.0)
-    divL(1.0, 0.0, 1.0, 0.0, 0.0, 0.0)
-    divLLap = divL + laplace
-    divLLap(1.0, 0.0, 1.0, 0.0, 0.0, 0.0)
-    a = ones(Float64, 3, 10)
-    a2 = [Vector(a[:, i]) for i in axes(a, 2)]
-    d(
-        [1.0, 1.0, 1.0],
-        [1.0, 1.0, 1.0],
-        0.0,
-        1.0,
-        6.0,
-        3.0,
-        0.01,
-        0.1111111111111111,
-        0.0,
-        0.0,
-        0.0,
-        0.0
-    )
-    divU(
-        [1.0, 1.0, 1.0],
-        [1.0, 1.0, 1.0],
-        0.0,
-        1.0,
-        6.0,
-        3.0,
-        0.01,
-        0.1111111111111111,
-        0.0,
-        0.0,
-        0.0,
-        0.0
-    )
-    divL(
-        ones(Float64, 3),
-        ones(Float64, 3),
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0
-    )
-    laplace(
-        [1.0, 1.0, 1.0],
-        [1.0, 1.0, 1.0],
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        0.0,
-        0.0,
-        0.0
-    )
-    faceBased(
-        Int32(1),
-        zeros(Int32, 1),
-        zeros(Int32, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(Int32, 1),
-        zeros(Float64, 1),
-        "Div{Float64, upwind}(upwind{Float64}(), 1.0)",
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-    )
-    faceBased(
-        Int32(1),
-        zeros(Int32, 1),
-        zeros(Int32, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(Int32, 1),
-        zeros(Float64, 1),
-        "Div{Float64, linear}(linear{Float64}(), 1.0)",
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-    )
-    faceBased(
-        Int32(1),
-        zeros(Int32, 1),
-        zeros(Int32, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(Int32, 1),
-        zeros(Float64, 1),
-        "Laplace{Float64}(1.0)",
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-    )
-    faceBased(
-        Int32(1),
-        zeros(Int32, 1),
-        zeros(Int32, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(Int32, 1),
-        zeros(Float64, 1),
-        "Div{Float64, linear}(linear{Float64}(), 1.0) + Laplace{Float64}(1.0)",
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-    )
-    faceBased(
-        Int32(1),
-        zeros(Int32, 1),
-        zeros(Int32, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(Int32, 1),
-        zeros(Float64, 1),
-        "Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0) + Laplace{Float64}(1.0)",
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-    )
-
-    faceBasedBoundary(
-        Int32(3),
-        ones(Int32, 3),
-        ones(UInt8, 3),
-        ones(Int32, 3),
-        zeros(Float64, 3),
-        "Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0)",
-        ones(Float64, 3),
-        ones(Float64, 3),
-        ones(Float64, 3),
-        ones(Float64, 3),
-        ones(Float64, 3),
-        ones(Float64, 3, 12),
-        ones(Float64, 3, 12),
-        ones(Float64, 3),
-    )
-    faceBasedBoundary(
-        Int32(3),
-        ones(Int32, 3),
-        ones(UInt8, 3),
-        ones(Int32, 3),
-        zeros(Float64, 3),
-        "Laplace{Float64}(1.0)",
-        ones(Float64, 3),
-        zeros(Float64, 3),
-        zeros(Float64, 3),
-        zeros(Float64, 3),
-        ones(Float64, 3),
-        zeros(Float64, 3, 12),
-        zeros(Float64, 3, 12),
-        zeros(Float64, 3),
-    )
-    # numInteriorFaces::Int32,
-    # owner::Vector{Int32},  # surfaceCells 
-    # diagOffs::Vector{UInt8},
-    # rowOffs::Vector{Int32},
-    # vals::Vector{Float64},
-    # opString::String,
-    # faceFlux::Vector{Float64},
-    # gamma::Vector{Float64},
-    # deltaCoeffs::Vector{Float64},
-    # magFaceArea::Vector{Float64},
-    # valueFractions::Vector{Float64},
-    # refValue_::Matrix{Float64},
-    # refGradient_::Matrix{Float64},
-    # RHS::Vector{Float64}
-    faceBasedBoundary(
-        Int32(3),
-        ones(Int32, 3),
-        ones(UInt8, 3),
-        ones(Int32, 3),
-        zeros(Float64, 3),
-        "Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0) + Laplace{Float64}(1.0)",
-        ones(Float64, 3),
-        zeros(Float64, 3),
-        zeros(Float64, 3),
-        zeros(Float64, 3),
-        zeros(Float64, 3),
-        zeros(Float64, 3, 12),
-        zeros(Float64, 3, 12),
-        zeros(Float64, 3),
-    )
-    faceBasedAll(
-        Int32(1),
-        zeros(Int32, 1),
-        zeros(Int32, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(UInt8, 1),
-        zeros(Int32, 1),
-        zeros(Float64, 1),
-        "Div{Float64, linear}(linear{Float64}(), 1.0) + Laplace{Float64}(1.0)",
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 1),
-        zeros(Float64, 3),
-        zeros(Float64, 3*12),
-        zeros(Float64, 3*12),
-        zeros(Float64, 3),
-        zeros(Int32, 4),
-    )
-
-    # numInteriorFaces::Int32,
-    # owner::Vector{Int32},
-    # neighbour::Vector{Int32},
-    # diagOffs::Vector{UInt8},
-    # ownOffs::Vector{UInt8},
-    # neiOffs::Vector{UInt8},
-    # rowOffs::Vector{Int32},
-    # vals::Vector{Float64},
-    # opString::String,
-    # faceFlux::Vector{Float64},
-    # gamma::Vector{Float64},
-    # deltaCoeffs::Vector{Float64},
-    # magFaceArea::Vector{Float64},
-    # valueFractions::Vector{Float64},
-    # refValue::Matrix{Float64},
-    # refGradient_::Matrix{Float64},
-    # RHS::Vector{Float64}
-    cstr = cstr = Base.cconvert(Cstring, "Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0) + Laplace{Float64}(1.0)")
-
-    GC.@preserve cstr begin
-        ptr = Base.unsafe_convert(Cstring, cstr)
-        test(
-            Int32(2),
-            ones(Int32, 4),
-            ones(Int32, 4),
-            ones(UInt8, 4),
-            ones(UInt8, 4),
-            ones(UInt8, 4),
-            ones(Int32, 4),
-            zeros(Float64, 4),
-            ptr,
-            ones(Float64, 4),
-            ones(Float64, 4),
-            ones(Float64, 4),
-            ones(Float64, 4),
-            ones(Float64, 4),
-            ones(Float64, 3, 12),
-            ones(Float64, 3, 12),
-            zeros(Float64, 12),
-        )
-    end
-
-    faceBasedAll(
-        Int32(2),
-        ones(Int32, 4),
-        ones(Int32, 4),
-        ones(UInt8, 4),
-        ones(UInt8, 4),
-        ones(UInt8, 4),
-        ones(Int32, 4),
-        zeros(Float64, 4),
-        "Div{Float64, upwind{Float64}}(upwind{Float64}(), 1.0) + Laplace{Float64}(1.0)",
-        ones(Float64, 4),
-        ones(Float64, 4),
-        ones(Float64, 4),
-        ones(Float64, 4),
-        ones(Float64, 4),
-        ones(Float64, 3*12),
-        ones(Float64, 3*12),
-        zeros(Float64, 12),
-        zeros(Int32, 4),
-    )
-    op = Div{Float64,upwind{Float64}}(upwind{Float64}(), 1.0)
-    a = ones(Float64, 3)
-    op(
-        a, a,
-        one(Float64),
-        one(Float64),
-        6.0,
-        3.0,
-        one(Float64),
-        one(Float64),
-        0.0, 0.0, 0.0, 0.0
-    )
+    warmup("Div{Float64, linear{Float64}}(linear{Float64}(), 1)")
+    warmup("Laplace{Float64}(1)")
+    warmup("Div{Float64, upwind{Float64}}(upwind{Float64}(), 1) + Laplace{Float64}(-5)")
+    warmup("Div{Float64, upwind{Float64}}(upwind{Float64}(), 1) + Laplace{Float64}(1)")
 
 end
 export faceBased, Div, Noop, linear, upwind, BDF1, Laplace, test, faceBasedBoundary, faceBasedAll
