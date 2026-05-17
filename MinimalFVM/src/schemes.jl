844b3f3fe3cd9e776a7741d5e7de4dc7435a9fbe
Lukas Petermann <lukas.petermann13@gmail.com>
2026-05-17 11:24:33 +0200

    add ddt to interface, start with cellbased

--- MinimalFVM/src/schemes.jl
+++ MinimalFVM/src/schemes.jl
@@ -12,11 +12,39 @@ struct linear{P} <: DivScheme{P} end
 #### DDT SCHEMES
 abstract type DdtScheme{P} end
 
-struct BDF1{P} <: DdtScheme{P} end
-@inline function (s::BDF1{P})(vol_c, Δt, oldVector::Vector{P},::Vector{P}) where {P<:AbstractFloat}
-    vol_c * 1 / Δt
-    return 
+struct BDF1{P} <: DdtScheme{P}
+    a0a1::P
 end
 
-struct BDF2{P} <: DdtScheme{P} end
-@inline (s::BDF2)(vol_c, Δt,) = vol_c * Δt
+function BDF1(Δt::P) where {P<:AbstractFloat}
+    return BDF1{P}(1 / Δt)
+end
+
+@inline function (s::BDF1{P})(volume::P, oldVector::Vector{P}) where {P<:AbstractFloat}
+    common = volume * s.a0a1
+    return common, common .* oldVector # diag, rhs 
+end
+struct BDF2{P} <: DdtScheme{P}
+    a0::P
+    a1::P
+    a2::P
+end
+DELTAT = nothing
+@inline function (s::BDF2)(volume::P, oldVector::Vector{P}, oldOldVector::Vector{P}) where {P<:AbstractFloat}
+    diag = volume * s.a0
+    rhs = volume * s.a1 .* oldVector[celli] + volume * s.a2 .* oldOldVector[celli]
+    return diag, rhs
+end
+function BDF2(Δt::P)::BDF2{P} where {P<:AbstractFloat}
+    return BDF2{P}(1.5 / Δt, 2.0 / Δt, -0.5 / Δt)
+end
+
+
+function BDF2(d::Nothing)::BDF2{Float64}
+    println("[BDF2] No Δt was passed to assemble.")
+    return BDF2{Float64}(1.0, 1.0, 1.0)
+end
+function BDF1(d::Nothing)::BDF1{Float64}
+    println("[BDF1] No Δt was passed to assemble.")
+    return BDF1{Float64}(1.0)
+end
