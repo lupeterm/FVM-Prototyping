2d6fd2c54686550a8ce2efd16df7c196627df811
Lukas Petermann <lukas.petermann13@gmail.com>
2026-05-20 15:14:00 +0200

    add gpu facebased interface

--- MinimalFVM/src/operators.jl
+++ MinimalFVM/src/operators.jl
@@ -1,4 +1,5 @@
 include("schemes.jl")
+using StaticArrays
 #### Transient Term 
 # ddt(ϕ)
 
@@ -72,8 +73,35 @@ function (d::Div{P,S})(
     valueRHSz::P
 ) where {P<:AbstractFloat,S}
     valFrac2 = 1.0 - valueFraction
-    v = flux * valueFraction .* refValue + valFrac2 .* refGradient / deltaCoeffGlobal
-    return valueDiag + flux * valFrac2 * d.scale, valueRHSx - v[1] * d.scale, valueRHSy - v[2] * d.scale, valueRHSz - v[3] * d.scale
+	valueRHSx -= flux * valueFraction * refValue[1] + valFrac2 * refGradient[1] / deltaCoeffGlobal
+    #valueRHSx, valueRHSy, valueRHSz = (valueRHSx, valueRHSy, valueRHSz) .- flux * valueFraction .* refValue + valFrac2 .* refGradient / deltaCoeffGlobal
+    return valueDiag + flux * valFrac2 * d.scale, valueRHSx, valueRHSy, valueRHSz 
+end
+
+# facebased boundary 
+function (d::Div{P,S})(
+    refValuex::P,
+    refValuey::P,
+    refValuez::P,
+    refGradientx::P,
+    refGradienty::P,
+    refGradientz::P,
+    flux::P,
+    valueFraction::P,
+    deltaCoeffGlobal::P,
+    _::P,
+    _::P,
+    _::P,
+    valueDiag::P,
+    valueRHSx::P,
+    valueRHSy::P,
+    valueRHSz::P
+) where {P<:AbstractFloat,S}
+    valFrac2 = 1.0 - valueFraction
+	valueRHSx -= flux * valueFraction * refValuex + valFrac2 * refGradientx / deltaCoeffGlobal
+	valueRHSx -= flux * valueFraction * refValuey + valFrac2 * refGradienty / deltaCoeffGlobal
+	valueRHSx -= flux * valueFraction * refValuez + valFrac2 * refGradientz / deltaCoeffGlobal
+    return valueDiag + flux * valFrac2 * d.scale, valueRHSx* d.scale, valueRHSy* d.scale, valueRHSz * d.scale
 end
 
 #### Diffusion
@@ -114,10 +142,37 @@ function (d::Laplace{P})(
 ) where {P<:AbstractFloat}
     flux = gamma * magFaceArea
     valueMat = flux * valueFraction * deltaCoeffGlobal
-    v = flux * (valueFraction * deltaCoeffGlobal .* refValue
-                +
-                (1.0 - valueFraction) .* refGradient)
-    return valueDiag - valueMat * d.scale, valueRHSx - v[1] * d.scale, valueRHSy - v[2] * d.scale, valueRHSz - v[3] * d.scale
+	valueRHSx, valueRHSy, valueRHSz = (valueRHSx, valueRHSy, valueRHSz) .-  (flux * (valueFraction * deltaCoeffGlobal .* refValue  +  (1.0 - valueFraction) .* refGradient)) * d.scale
+    return valueDiag - valueMat * d.scale, valueRHSx, valueRHSy, valueRHSz
+end
+
+# facebased boundary 
+function (d::Laplace{P})(
+    refValuex::P,
+    refValuey::P,
+    refValuez::P,
+    refGradientx::P,
+    refGradienty::P,
+    refGradientz::P,
+    _::P,
+    valueFraction::P,
+    deltaCoeffGlobal::P,
+    deltaCoeffBoundary::P,
+    gamma::P,
+    magFaceArea::P,
+    valueDiag::P,
+    valueRHSx::P,
+    valueRHSy::P,
+    valueRHSz::P
+) where {P<:AbstractFloat}
+    flux = gamma * magFaceArea
+    valueMat = flux * valueFraction * deltaCoeffGlobal
+    vd = valueFraction * deltaCoeffGlobal
+    relFraction = 1-valueFraction
+	tmpx = d.scale * flux * (refValuex * vd + refGradientx * relFraction)
+    tmpy = d.scale * flux * (refValuey * vd + refGradienty * relFraction)
+    tmpz = d.scale * flux * (refValuez * vd + refGradientz * relFraction)
+    return valueDiag - valueMat * d.scale, valueRHSx -tmpx, valueRHSy-tmpy, valueRHSz-tmpz
 end
 
 #### Operator Fusing
@@ -152,7 +207,24 @@ end
     valueDiag, valueRHSx, valueRHSy, valueRHSz = o.b(refValue, refGradient, flux, valueFraction, deltaCoeffGlobal, deltaCoeffBoundary, gamma, magFaceArea, valueDiag, valueRHSx, valueRHSy, valueRHSz)
     return valueDiag, valueRHSx, valueRHSy, valueRHSz
 end
-
+@inline function (o::DiffEq)(
+    refValuex,refValuey,refValuez,
+    refGradientx,refGradienty,refGradientz,
+    flux,
+    valueFraction,
+    deltaCoeffGlobal,
+    deltaCoeffBoundary,
+    gamma,
+    magFaceArea,
+    valueDiag,
+    valueRHSx,
+    valueRHSy,
+    valueRHSz
+)
+    valueDiag, valueRHSx, valueRHSy, valueRHSz = o.a(refValuex,refValuey,refValuez, refGradientx,refGradienty,refGradientz, flux, valueFraction, deltaCoeffGlobal, deltaCoeffBoundary, gamma, magFaceArea, valueDiag, valueRHSx, valueRHSy, valueRHSz)
+    valueDiag, valueRHSx, valueRHSy, valueRHSz = o.b(refValuex,refValuey,refValuez, refGradientx,refGradienty,refGradientz, flux, valueFraction, deltaCoeffGlobal, deltaCoeffBoundary, gamma, magFaceArea, valueDiag, valueRHSx, valueRHSy, valueRHSz)
+    return valueDiag, valueRHSx, valueRHSy, valueRHSz
+end
 # cellbased inner 
 # @inline function (DiffEq)(U_c, U_n, Sf, nu, gdiff, volume, dt, valueUpper, valueDiag, valueLower)
 #     valueUpper, valueDiag, valueLower = o.a(U_c, U_n, Sf, nu, gdiff, volume, dt, valueUpper, valueDiag, valueLower)
